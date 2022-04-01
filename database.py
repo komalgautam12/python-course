a97f709be5d7cd08d616fcb4a56cdd35928b080c
komalgautam12 <78651827+komalgautam12@users.noreply.github.com>
2022-04-01 01:46:22 -0700

    database

--- database.py
+++ database.py
@@ -1,8 +1,8 @@
-from email.policy import default
+
 from sqlalchemy.engine import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column,Integer,String,Float,DateTime
-from datetime import datatime, datetime
+from datetime import datetime
 Base =declarative_base()
 
 class Student(Base):
@@ -19,10 +19,13 @@ class Product(Base):
     quantity=Column(Integer,default=0)
     brand=-Column(String(50))
     detail=Column (String ,default='')
-    mfg=Column(datetime,default=datetime.now)
-    added_on=Column(datetime,default=datetime.now)
+    mfg=Column(DateTime,default=datetime.now)
+    added_on=Column(DateTime,default=datetime.now)
 
-engine= create_engine("sqlite:///school.sqlite")
+
+engine= create_engine("sqlite:///school.sqlite",echo=True)
+
+
 Base.metadata.create_all(engine)
 
 
