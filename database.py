fa60cfc12999e67975e5720f5c18dec0dfe7dcd9
komalgautam12 <78651827+komalgautam12@users.noreply.github.com>
2022-04-01 01:28:00 -0700

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,8 @@
+from email.policy import default
 from sqlalchemy.engine import create_engine
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column,Integer,String,Float
+from sqlalchemy import Column,Integer,String,Float,DateTime
+from datetime import datatime, datetime
 Base =declarative_base()
 
 class Student(Base):
@@ -9,6 +11,18 @@ class Student(Base):
     name=Column (String(50))
     klass=Column(Integer)
 
+class Product(Base):
+    __tablename__='products'
+    id=Column (Integer,primary_key=True)
+    title=Column(String,unique=True)
+    mrp=Column(Float,default=0.0)
+    quantity=Column(Integer,default=0)
+    brand=-Column(String(50))
+    detail=Column (String ,default='')
+    mfg=Column(datetime,default=datetime.now)
+    added_on=Column(datetime,default=datetime.now)
 
 engine= create_engine("sqlite:///school.sqlite")
 Base.metadata.create_all(engine)
+
+
