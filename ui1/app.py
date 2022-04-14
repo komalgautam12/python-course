348cde6874d33bc38029ef9090cbd62e688b9d4b
komalgautam12 <komalgautam2731@gmail.com>
2022-04-14 08:49:19 +0530

    form

--- ui1/app.py
+++ ui1/app.py
@@ -1,12 +1,25 @@
-
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,flash,redirect
 app = Flask(__name__)
-
-
-@app.route('/',methods=['GET','POST'])
+app.secret_key='thisisverysecretkey'
+@app.route('/', methods=['GET','POST'])
 def index():
     if request.method=='POST':
-      print(request.form)
+      question=request.form.get("question")
+      op1=request.form.get("op1")
+      op2=request.form.get("op2")
+      op3=request.form.get("op3")
+      op4=request.form.get("op4")
+      ans=request.form.get("ans")
+      category=request.form.get("category")
+      if  not question or len(question) <10:
+        flash("Question is required and should be at least 10 characeter long", "danger")
+        return redirect('/')
+      elif not op1:
+          flash ("Option 1 is required","danger")
+          return redirect("/")
+      else :
+        flash("question added successfully","success")
+        return redirect("/")
     return render_template('index.html') 
 @app.route('/about')
 def about():
